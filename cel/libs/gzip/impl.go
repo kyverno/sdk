fbd965d5c27a6d31d365bcfabe1debb90e94aade
aerosouund <aerosound161@gmail.com>
2026-03-18 21:14:50 +0200

    add gzip library with compress and decompress functionality
    Signed-off-by: aerosouund <aerosound161@gmail.com>

---
+++ cel/libs/gzip/impl.go
@@ -0,0 +1,51 @@
+package gzip
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+
+	"github.com/google/cel-go/common/types"
+	"github.com/google/cel-go/common/types/ref"
+	"github.com/kyverno/sdk/cel/utils"
+)
+
+type impl struct {
+	types.Adapter
+}
+
+func (i *impl) decompress_string(gzBytes ref.Val) ref.Val {
+	if data, err := utils.ConvertToNative[[]byte](gzBytes); err != nil {
+		return types.WrapErr(err)
+	} else {
+		r, err := gzip.NewReader(bytes.NewReader(data))
+		if err != nil {
+			return types.WrapErr(err)
+		}
+		defer r.Close()
+
+		out, err := io.ReadAll(r)
+		if err != nil {
+			return types.WrapErr(err)
+		}
+
+		return i.NativeToValue(string(out))
+	}
+}
+
+func (i *impl) compress(value ref.Val) ref.Val {
+	if native, err := utils.ConvertToNative[string](value); err != nil {
+		return types.WrapErr(err)
+	} else {
+		var buf bytes.Buffer
+		w := gzip.NewWriter(&buf)
+
+		_, err = w.Write([]byte(native))
+		if err != nil {
+			return types.WrapErr(err)
+		}
+
+		w.Close()
+		return i.NativeToValue(buf.Bytes())
+	}
+}
