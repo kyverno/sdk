6684fd914633a8187fbfd66cf2d636d141285605
aerosouund <aerosound161@gmail.com>
2026-03-20 16:30:48 +0200

    return nil in the namespaced imp
    Signed-off-by: aerosouund <aerosound161@gmail.com>

--- cel/libs/resource/namespaced_impl.go
+++ cel/libs/resource/namespaced_impl.go
@@ -98,7 +98,7 @@ func (c *namespacedImpl) get_resource_string_string_string(args ...ref.Val) ref.
 		res, err := self.GetResource(apiVersion, resource, c.namespace, name)
 		if err != nil {
 			if apierrors.IsNotFound(err) {
-				return c.NativeToValue(res.UnstructuredContent())
+				return types.NullValue
 			}
 			if apierrors.IsForbidden(err) || apierrors.IsUnauthorized(err) {
 				return types.NewErr("failed to get resource: permission denied: %v", err)
